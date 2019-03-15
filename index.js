338e90fbf8e965f6f6d3bc566100a897537f3306
Luka <barasmaler@gmail.com>
2019-03-15 18:19:28 +0100

    Migration and seed implemented

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const routes = require('./routes/routes.js');
 const PORT = process.env.PORT || 5000;
+
+
+routes(router);
+app.listen(PORT,() => console.log(`Server running on ${PORT}`));
 
 
 
---
+++ routes/ruotes.js
@@ -0,0 +1,5 @@
+module.exports = (router) =>{
+    const books = require('../controllers/books');
+
+    //router.get(books.getAllBooks);
+}
